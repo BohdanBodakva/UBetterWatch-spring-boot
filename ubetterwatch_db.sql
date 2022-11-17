2ea49c9c823e9b3fcd6394a3c4bcaac4f1f0bd68
Bohdan Bodakva <bodakvabohdan44@gmail.com>
2022-11-17 19:02:50 +0200

    db

--- ubetterwatch_db.sql
+++ ubetterwatch_db.sql
@@ -1,86 +1,68 @@
--- MySQL Workbench Forward Engineering
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
 -- -----------------------------------------------------
 -- Schema ubetterwatch_db
 -- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema ubetterwatch_db
--- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `ubetterwatch_db`;
-CREATE SCHEMA `ubetterwatch_db` DEFAULT CHARACTER SET utf8 ;
+DROP DATABASE IF EXISTS `ubetterwatch_db`;
+CREATE DATABASE `ubetterwatch_db`;
 USE `ubetterwatch_db` ;
 
-
--- DROP TABLE IF EXISTS `ubetterwatch_db`.`supervisor`;
--- DROP TABLE IF EXISTS `ubetterwatch_db`.`user`;
--- DROP TABLE IF EXISTS `ubetterwatch_db`.`bracelet`;
--- DROP TABLE IF EXISTS `ubetterwatch_db`.`coordinates`;
--- DROP TABLE IF EXISTS `ubetterwatch_db`.`bracelet_data`;
--- DROP TABLE IF EXISTS `ubetterwatch_db`.`voice_messages`;
--- DROP TABLE IF EXISTS `ubetterwatch_db`.`bracelet_storage`;
-
+DROP TABLE IF EXISTS `ubetterwatch_db`.`bracelet_storage`;
+DROP TABLE IF EXISTS `ubetterwatch_db`.`voice_messages`;
+DROP TABLE IF EXISTS `ubetterwatch_db`.`coordinates`;
+DROP TABLE IF EXISTS `ubetterwatch_db`.`bracelet`;
+DROP TABLE IF EXISTS `ubetterwatch_db`.`bracelet_data`;
+DROP TABLE IF EXISTS `ubetterwatch_db`.`user`;
+DROP TABLE IF EXISTS `ubetterwatch_db`.`supervisor`;
 
 -- -----------------------------------------------------
 -- Table `ubetterwatch_db`.`supervisor`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ubetterwatch_db`.`supervisor` (
+CREATE TABLE `ubetterwatch_db`.`supervisor` (
   `username` VARCHAR(100) NOT NULL,
   `first_name` VARCHAR(45) NOT NULL,
   `last_name` VARCHAR(45) NOT NULL,
   `password` VARCHAR(100) NOT NULL,
   `role` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`username`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
+  PRIMARY KEY (`username`)
+);
 
 
 -- -----------------------------------------------------
 -- Table `ubetterwatch_db`.`user`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ubetterwatch_db`.`user` (
+CREATE TABLE `ubetterwatch_db`.`user` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(45) NOT NULL,
   `last_name` VARCHAR(45) NOT NULL,
   `birth_date` DATE NOT NULL,
-  `start_use_date` TIMESTAMP NOT NULL,
-  `supervisor_username` VARCHAR(100) NOT NULL,
+  `start_use_date` TIMESTAMP,
+  `supervisor_username` VARCHAR(100),
   PRIMARY KEY (`id`),
   INDEX `fk_user_supervisor1_idx` (`supervisor_username` ASC) VISIBLE,
   CONSTRAINT `fk_user_supervisor1`
     FOREIGN KEY (`supervisor_username`)
-    REFERENCES `ubetterwatch_db`.`supervisor` (`username`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 9
-DEFAULT CHARACTER SET = utf8mb3;
+    REFERENCES `ubetterwatch_db`.`supervisor` (`username`)
+    ON DELETE SET NULL
+    ON UPDATE SET NULL
+);
 
 
 -- -----------------------------------------------------
 -- Table `ubetterwatch_db`.`bracelet_data`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ubetterwatch_db`.`bracelet_data` (
+CREATE TABLE `ubetterwatch_db`.`bracelet_data` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `sim_name` VARCHAR(45) NOT NULL,
-  `apn` VARCHAR(70) NOT NULL,
-  `username` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 5
-DEFAULT CHARACTER SET = utf8mb3;
+  `sim_name` VARCHAR(45),
+  `apn` VARCHAR(70), 
+  `username` VARCHAR(45),
+  `password` VARCHAR(45),
+  PRIMARY KEY (`id`)
+);
 
 
 -- -----------------------------------------------------
 -- Table `ubetterwatch_db`.`bracelet`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ubetterwatch_db`.`bracelet` (
+CREATE TABLE `ubetterwatch_db`.`bracelet` (
   `serial_number` VARCHAR(50) NOT NULL,
   `user_id` BIGINT NOT NULL,
   `bracelet_data_id` BIGINT NOT NULL,
@@ -89,30 +71,30 @@ CREATE TABLE IF NOT EXISTS `ubetterwatch_db`.`bracelet` (
   INDEX `fk_bracelet_bracelet_data1_idx` (`bracelet_data_id` ASC) VISIBLE,
   CONSTRAINT `fk_bracelet_user1`
     FOREIGN KEY (`user_id`)
-    REFERENCES `ubetterwatch_db`.`user` (`id`),
+    REFERENCES `ubetterwatch_db`.`user` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
   CONSTRAINT `fk_bracelet_bracelet_data1`
     FOREIGN KEY (`bracelet_data_id`)
     REFERENCES `ubetterwatch_db`.`bracelet_data` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
 
 
 -- -----------------------------------------------------
 -- Table `ubetterwatch_db`.`bracelet_storage`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ubetterwatch_db`.`bracelet_storage` (
+CREATE TABLE `ubetterwatch_db`.`bracelet_storage` (
   `serial_number` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`serial_number`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
+  PRIMARY KEY (`serial_number`)
+);
 
 
 -- -----------------------------------------------------
 -- Table `ubetterwatch_db`.`coordinates`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ubetterwatch_db`.`coordinates` (
+CREATE TABLE `ubetterwatch_db`.`coordinates` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `x` VARCHAR(200) NOT NULL,
   `y` VARCHAR(200) NOT NULL,
@@ -122,15 +104,16 @@ CREATE TABLE IF NOT EXISTS `ubetterwatch_db`.`coordinates` (
   INDEX `fk_coordinates_bracelet1_idx` (`bracelet_serial_number` ASC) VISIBLE,
   CONSTRAINT `fk_coordinates_bracelet1`
     FOREIGN KEY (`bracelet_serial_number`)
-    REFERENCES `ubetterwatch_db`.`bracelet` (`serial_number`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
+    REFERENCES `ubetterwatch_db`.`bracelet` (`serial_number`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
 
 
 -- -----------------------------------------------------
 -- Table `ubetterwatch_db`.`voice_messages`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ubetterwatch_db`.`voice_messages` (
+CREATE TABLE `ubetterwatch_db`.`voice_messages` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `file_name` VARCHAR(120) NOT NULL,
   `time` TIMESTAMP NOT NULL,
@@ -139,12 +122,17 @@ CREATE TABLE IF NOT EXISTS `ubetterwatch_db`.`voice_messages` (
   INDEX `fk_voice_messages_bracelet1_idx` (`bracelet_serial_number` ASC) VISIBLE,
   CONSTRAINT `fk_voice_messages_bracelet1`
     FOREIGN KEY (`bracelet_serial_number`)
-    REFERENCES `ubetterwatch_db`.`bracelet` (`serial_number`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 5
-DEFAULT CHARACTER SET = utf8mb3;
+    REFERENCES `ubetterwatch_db`.`bracelet` (`serial_number`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
 
+insert into supervisor(username, first_name, last_name, password, role) values('admin', 'admin', 'admin', '$2a$10$xh2fvb9Kh8vr9yQ9JZ2.8.6H7oygVXrihrF/GKWWCpE21p2qc0r4i', 'ROLE_ADMIN'
+);
+insert into supervisor(username, first_name, last_name, password, role) values('s1', 's1', 's1', '$2a$10$HRTvJznxENS4ahwoKDP6d.B67AR9J.xu7gl87eJ8p/QNFoLoJ/UaC', 'ROLE_SUPERVISOR'
+);
+insert into supervisor(username, first_name, last_name, password, role) values('s2', 's2', 's2', '$2a$10$qfP0L7nKVqGKlZlAJhT3G.7i/95pio4fNDBDo09/zZoYHmsQe19y2', 'ROLE_SUPERVISOR'
+);
+insert into supervisor(username, first_name, last_name, password, role) values('s3', 's3', 's3', '$2a$10$qB81aAxZiuyMVm4ly3By9.vdhQnyJVMudMQCJOXJwg.5Arof9k76i', 'ROLE_SUPERVISOR'
+);
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
