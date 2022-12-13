62f7f3d2e33e777ef3a9d616a14b2d7529917be4
Bohdan Bodakva <bodakvabohdan44@gmail.com>
2022-12-13 10:49:09 +0200

    gbh

--- src/main/java/ua/lviv/iot/ubetterwatch/configuration/SecurityConfiguration.java
+++ src/main/java/ua/lviv/iot/ubetterwatch/configuration/SecurityConfiguration.java
@@ -1,79 +1,75 @@
-package ua.lviv.iot.ubetterwatch.configuration;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import ua.lviv.iot.ubetterwatch.service.implementation.SupervisorDetailsService;
-
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    private final SupervisorDetailsService supervisorDetailsService;
-
-    @Autowired
-    public SecurityConfiguration(SupervisorDetailsService supervisorDetailsService) {
-        this.supervisorDetailsService = supervisorDetailsService;
-    }
-
-    // paths for Swagger
-    private static final String[] SWAGGER_PATHS = {
-            // -- Swagger UI v2
-            "/v2/api-docs",
-            "/swagger-resources",
-            "/swagger-resources/**",
-            "/configuration/ui",
-            "/configuration/security",
-            "/swagger-ui.html",
-            "/webjars/**",
-            // -- Swagger UI v3 (OpenAPI)
-            "/v3/api-docs/**",
-            "/swagger-ui/**"
-    };
-
-    // configs the authorization,
-    // and Spring Security in general
-    @Override
-    protected void configure(HttpSecurity http) throws Exception{
-
-        http.authorizeRequests()
-                // requests
-                .antMatchers("/api/admin/**").hasRole("ADMIN")
-                .antMatchers("/api/supervisors/**").hasRole("SUPERVISOR")
-                .antMatchers("/api/auth/login", "/api/auth/registration", "/error").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                // login
-                .formLogin().loginPage("/api/auth/login")
-                .loginProcessingUrl("/process_login")
-                .defaultSuccessUrl("/api/default/", true)
-                .failureUrl("/api/auth/login?error")
-                .and()
-                // logout
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/api/auth/logout"))
-                .logoutSuccessUrl("/api/auth/login");
-
-        http.csrf().disable();
-    }
-
-    // configs the authentication
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(supervisorDetailsService)
-                .passwordEncoder(getPasswordEncoder());
-    }
-
-    // password encoding
-    @Bean
-    public PasswordEncoder getPasswordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-
-}
+//package ua.lviv.iot.ubetterwatch.configuration;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+//import ua.lviv.iot.ubetterwatch.security.jwt.JwtConfigurer;
+//import ua.lviv.iot.ubetterwatch.security.jwt.JwtTokenProvider;
+//import ua.lviv.iot.ubetterwatch.service.implementation.SupervisorDetailsService;
+//
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//    private final SupervisorDetailsService supervisorDetailsService;
+////    private final JwtTokenProvider jwtTokenProvider;
+//
+//    @Autowired
+//    public SecurityConfiguration(SupervisorDetailsService supervisorDetailsService, JwtTokenProvider jwtTokenProvider) {
+//        this.supervisorDetailsService = supervisorDetailsService;
+////        this.jwtTokenProvider = jwtTokenProvider;
+//    }
+//
+////    @Bean
+////    @Override
+////    public AuthenticationManager authenticationManagerBean() throws Exception{
+////        return super.authenticationManagerBean();
+////    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception{
+//
+//        http.csrf().disable()
+//                .httpBasic().disable()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers("/api/oleg").permitAll()
+//                .antMatchers("/api/admin/**").hasRole("ADMIN")
+//                .antMatchers("/api/supervisors/**").hasRole("SUPERVISOR")
+//                .antMatchers("/api/auth/login", "/api/auth/registration", "/error").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+////                .apply(new JwtConfigurer(jwtTokenProvider))
+////                .and()
+//                .formLogin().loginPage("/api/auth/login")
+//                .loginProcessingUrl("/process_login")
+//                .defaultSuccessUrl("/api/default/", true)
+//                .failureUrl("/api/auth/login?error")
+//                .and()
+//                .logout()
+//                .logoutRequestMatcher(new AntPathRequestMatcher("/api/auth/logout"))
+//                .logoutSuccessUrl("/api/auth/login");
+//
+//    }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(supervisorDetailsService)
+//                .passwordEncoder(getPasswordEncoder());
+//    }
+//
+//    @Bean
+//    public PasswordEncoder getPasswordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//}
